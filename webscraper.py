f32393149b37f8275ac512c999c85b65a3fc6615
periadhityan <44988530+periadhityan@users.noreply.github.com>
2024-09-30 16:37:20 +0800

    Added html files for PLAY group, scraped the data and collected the countries count for comparison on jupyternotebook

--- webscraper.py
+++ webscraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import csv
+import os
 
 def RansomHub():
 
@@ -29,5 +30,49 @@ def RansomHub():
         writer = csv.writer(f, delimiter=',')
         writer.writerows(info)
 
-    
 
+    
+def Play():
+
+    directory = 'Play_HTML'
+    data = []
+    for filename in os.listdir(directory):
+        print(filename)
+        with open(f'{directory}/{filename}', 'r') as f:
+            contents = f.read()
+            soup = BeautifulSoup(contents, "html.parser")
+            boxes = soup.find_all('th', class_='News')
+
+            for box in boxes:
+                info = box.find('div').text
+                info = info.split()
+
+                if(info[0] == 'United' and info[1] == 'States'):
+                    country = 'United States'
+                    info.pop(0)
+                    info.pop(0)        
+                elif(info[0] == 'United' and info[1] == 'Kingdom'):
+                    country = 'United Kingdom'
+                    info.pop(0)
+                    info.pop(0)
+                else:
+                    country = info[0]
+                    info.pop(0)
+
+                victim =  ''.join(filter(str.isalnum, info[0]))
+
+                info.pop(0)
+                date = ""
+                group = 'PLAY'
+                for char in info[2]:
+                    if(char.isalpha()):
+                        continue
+                    date += char
+                cell = [date, victim, country, group]
+                data.append(cell)
+
+    with open("PLAY.csv", "w", newline='') as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerows(data)
+
+Play()
